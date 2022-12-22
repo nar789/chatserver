929f5a609d6b8499078396477f0d42dd7b64c376
nar789 <admin@hume.co.kr>
2022-12-22 16:42:12 +0900

    init

---
+++ client/src/App.js
@@ -0,0 +1,400 @@
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+import './App.css';
+
+
+const socket = io.connect('http://localhost:1225')
+
+function User(id, name) {
+  this.id = id;
+  this.name = name;
+}
+
+function Message(id, name, message, image, updated, readCount) {
+  this.id = id;
+  this.name = name;
+  this.message = message; 
+  this.image = image; 
+  this.updated = updated;
+  this.readCount = readCount;
+}
+
+function Conversation(id, thumb, title, unreads, userCount, lastMessageId) {
+  this.id = id;
+  this.thumb = thumb;
+  this.title = title;
+  this.unreads = unreads;
+  this.userCount = userCount;
+  this.lastMessageId = lastMessageId;
+}
+
+let info = [
+  {
+    id : 1,
+    name : "아이유",
+    profile : "iu.PNG"
+  },
+  {
+    id : 2,
+    name : "유인나",
+    profile : "ui.PNG"
+  }
+];
+
+let lastUpdated = [];
+let copyMsg = [];
+let copyConvItem;
+let myId = 1; 
+
+function App() {
+  let [select, setSelect] = useState(new User(1, "아이유"));
+  let [chatList, setChatList] = useState([]);
+  let [convItem, setConvItem] = useState(new Conversation(0, "", "", 0, 0));
+  let [msg, setMsg] = useState([]);
+  let [imgPath, setImgPath] = useState("");
+  
+  useEffect(()=>{
+    init();
+  }, []);
+  
+
+
+  return (
+    <div className="App">
+      
+      <div>
+        <div>
+          <p>선택된 유저 : {select != null ? select.name : ""}</p>
+
+        </div>
+        <p>UsersList</p>
+          <button onClick={()=>changeUser(1, "아이유")}>아이유</button>
+          <button onClick={()=>changeUser(2, "유인나")}>유인나</button>
+      </div>
+      
+      <br></br>
+      <hr></hr>
+      <br></br>
+      
+      <button onClick={()=>createConversation()}>채팅방 DB 생성</button>
+      <div id = "chatjson">
+        <p>[채팅방 유저정보] <button onClick={()=>showChatJson()}>보기</button></p>
+      </div>
+
+      <br></br>
+      <hr></hr>
+      <br></br>
+
+      <div>
+        <p>채팅방 목록 <button onClick={()=>loadConversation()}>불러오기</button></p>
+        <ul id="chatlist">
+          {
+          chatList.map( (item, idx) => {
+
+            console.log(item);
+
+
+            return (
+              <div key={item.id}>
+                <li style={{display:'inline'}} onClick={()=>openChat(item)} >id : {item.id} / thumbnail : {item.thumbnail} / title : {item.title} / userCount : {item.user_count} / unreads : {item.unreads}</li>
+                &nbsp;&nbsp;<button onClick={()=>outConversation(item, idx)}>나가기</button>
+              </div>
+            );
+          })
+          }
+        </ul>
+      </div>
+      
+      
+      <br></br>
+      <hr></hr>
+      <br></br>
+      
+      <div>
+        <p>접속된 채팅방 : [{convItem.id}]{convItem.title} // 내 이름 : {select.name}</p>
+        <input type="text" id="message"></input><button onClick={()=>send()}>전송</button>
+        <hr />
+        <br></br>&nbsp;&nbsp;<input type="file" onChange={uploadImage.bind(this)} />
+        <br></br>
+        <button onClick={()=>setImgPath('')}>이미지 선택 취소</button>
+        <img src={"/img/" + imgPath} alt='preview' width={50} />
+        <br></br><br></br>
+        <center><button style={{display : `${imgPath === "" ? 'none': 'block'}`}} onClick={()=>sendImage()}>이미지 전송</button></center>
+        <hr />
+        <ul id="chat">
+          {
+            msg.map( (item, idx) => {
+              
+              return (
+                <li key={idx}>{item.name} : <img width={100} style={{display : `${item.image == null ? 'none' : 'inline-block'}`}} src={"/img/" + item.image} alt={idx} />{item.image == null ? item.message : ""} <small>{item.updated}</small> <small>unreads:{item.readCount}</small></li>
+              );
+
+            })
+          }
+        </ul>
+      </div>
+
+
+    </div>
+  );
+
+  function sendImage() {
+    if(convItem.id === 0) {
+      alert('채팅방을 선택해주세요.');
+      return;
+    }
+    
+    if(imgPath === "") {
+      return;
+    }
+  
+    let messageType = 2;
+    let updated = parseInt(Date.now()*0.001);
+
+    console.log('send image message!');
+
+    socket.emit("message", ({
+      conversationId : copyConvItem.id, 
+      userId : myId,
+      name : select.name,
+      message : '',
+      image : imgPath,
+      messageType : messageType, 
+      updated : updated
+    }));
+
+    setImgPath("");
+  }
+
+  function uploadImage(event) {
+    const files = event.target.files;
+    console.log(files[0].name);
+    socket.emit('uploadImage', ({file : files[0], name : files[0].name}));
+  }
+
+
+  function outConversation(item, idx) {
+    chatList.splice(idx, 1);
+    let newList = [];
+    chatList.map((value, idx) => {
+      newList.push(value);
+    });
+    setChatList(newList);
+
+    const userId = myId;
+    const conversationId = item.id;
+    const name = select.name;
+
+    socket.emit("outconversation", ({
+      userId : userId,
+      name : name,
+      conversationId : conversationId
+    }));
+
+    //socket emit out conversation
+    /**
+     * if(usercount != 1) {
+     *  update conversation name and user count
+     *  remove user_conversation where userid is myid
+     *  emit out message
+     * } else {
+     *    remove conversation where conv id = convid
+     *    remove user_conversation where conv id = convId
+     * }
+     */
+  }
+
+  function changeUser(id, name) {
+    setSelect(new User(id, name));
+    myId = id;
+    loadConversation();
+  } 
+
+
+  function openChat(item) {
+    if(convItem.id !== 0 && item.id !== convItem.id) {
+      socket.emit('leave', ({room : convItem.id}));
+    }
+    setConvItem(new Conversation(item.id, item.thumbnail, item.title, item.unreads, item.user_count, item.last_message_id));
+    copyConvItem = new Conversation(item.id, item.thumbnail, item.title, item.unreads, item.user_count, item.last_message_id);
+    socket.emit('join', ({room : item.id}));
+    setMsg([]);
+    copyMsg = [];
+    socket.emit('readinfo', ({
+      conversationId : copyConvItem.id,
+      userId : myId
+    }));
+
+    socket.emit('loadmessage', ({
+      conversationId : copyConvItem.id,
+      offset:0,
+      size:5
+    }));
+
+    socket.emit('loadreadinfo', ({
+      conversationId : copyConvItem.id
+    }));
+    
+
+    console.log('open chat~!!');
+  }
+
+  function loadConversation() {
+    socket.emit("loadConversation",({
+      userId : myId
+    }));
+  }
+
+
+  function send() {
+    if(convItem.id === 0) {
+      alert('채팅방을 선택해주세요.');
+      return;
+    }
+    let text = document.getElementById('message').value;
+    document.getElementById('message').value = "";
+    if(text === "") {
+      return;
+    }
+    
+  
+    let messageType = 1;
+    let updated = parseInt(Date.now()*0.001);
+
+    console.log('send message!');
+
+    socket.emit("message", ({
+      conversationId : copyConvItem.id, 
+      userId : myId,
+      name : select.name,
+      message : encodeURIComponent(text),
+      messageType : messageType, 
+      updated : updated
+    }));
+  }
+
+  function showChatJson() {
+    let text = document.getElementById('chatjson').innerHTML;
+    var infoStr = JSON.stringify(info);
+    document.getElementById('chatjson').innerHTML = text + infoStr;
+  }
+
+  function createConversation() {
+    var infoStr = JSON.stringify(info);
+    socket.emit("createConversation",({
+      info : infoStr
+    }));
+    console.log("create conversation~!");
+
+  }
+
+
+  function init() {
+    socket.on('welcome',() => {
+      console.log('welcome~~!');
+    });
+
+    socket.on('createConversation',({result})=>{
+      alert(result);
+    });
+
+    socket.on('loadConversation', ({data, result}) => {
+      if(result === 'fail') {
+        alert(result);
+        return;
+      }
+      const rows = JSON.parse(data);
+      setChatList(rows);
+    });
+
+
+    socket.on('message', ({messageId, conversationId, userId, name, message, image, messageType, updated}) => {
+      
+      let text = decodeURIComponent(message);
+      if(messageType === 3) {
+        text = `${name}님이 퇴장했습니다.`;
+        name = 'system';
+      }
+
+      
+      setMsg(msg => [...msg, new Message(messageId, name , text, image, updated)]);
+      copyMsg.push(new Message(messageId, name , text, image, updated, convItem.userCount));
+      socket.emit('readinfo', ({
+        conversationId : conversationId,
+        userId : myId,
+        lastMessageId : messageId
+      }));
+    });
+
+
+    socket.on('readinfo', ({conversationId, userId, lastMessageId}) => {
+      console.log(`${userId} readed, last msg id = ` + lastMessageId);
+      let findout = false;
+      for(var item of lastUpdated) {
+        if(item.userId === userId) {
+          item.updated = lastMessageId;
+          findout = true;
+        }
+      }
+      if(!findout) {
+        lastUpdated.push({userId : userId, updated : lastMessageId});
+      }
+      let newMsg = [];
+      for(let item of copyMsg) {
+        let sum = 0;
+        for(let updateItem of lastUpdated) {
+          if(updateItem.updated >= item.id) {
+            sum+=1;
+          }
+        }
+        if (item.name === 'system') {
+          item.readCount = 0;
+        } else {
+          item.readCount = copyConvItem.userCount - sum;
+        }
+        newMsg.push(item);
+      }
+      setMsg(newMsg);
+      
+    });
+
+
+    socket.on('loadmessage', ({rows}) => {
+      
+      let preload = [];
+      
+      for(let i=rows.length-1;i>=0;i--) {
+        let row = rows[i];
+        let text = decodeURIComponent(row.message);
+        let name = row.name;
+        if(row.message_type === 3) {
+          text = `${row.name}님이 퇴장했습니다.`;
+          name = 'system';
+        }
+
+        preload.push(new Message(row.id, name, text, row.image, row.updated, convItem.userCount));
+        copyMsg.push(new Message(row.id, name, text, row.image, row.updated, convItem.userCount));
+      }
+      setMsg(preload);
+    });
+
+
+    socket.on('uploadImage', ({filename}) => {
+
+      setImgPath(filename);
+
+    });
+
+
+  }
+
+
+
+
+
+
+}
+
+
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,266 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const cors = require('cors');
+const { writeFileSync } = require('fs');
+const io = require('socket.io')(server,{
+    cors : {
+        origin :"*",
+        credentials :true
+    }
+});
+const mysql = require('mysql2'); 
+const pool = mysql.createPool({
+    host : 'localhost',
+    user : 'root',
+    password : 'black',
+    database : 'chat'
+});
+
+
+io.on('connection', async socket =>{
+    socket.emit('welcome');
+
+    socket.on('createConversation',async ({info}) => {
+        await createConversation(socket, info);
+    });
+
+    socket.on('loadConversation', async({userId}) => {
+        loadConversation(socket, userId);
+    });
+
+    socket.on('join', ({room}) => {
+        socket.join(room);
+        console.log(socket.id + " is joined " + room);
+    });
+
+    socket.on('leave', ({room}) => {
+        socket.leave(room);
+        console.log(socket.id + " is leaved " + room);
+    });
+
+    socket.on('message',async ({conversationId, userId, name, message, image, messageType, updated}) => {
+
+        const conn = pool.promise();
+        let messageId = 0;
+        try {
+            const [result,fields] = await conn.query(`INSERT INTO message VALUES(null, ${conversationId}, ${userId}, ${messageType}, '${message}', from_unixtime(${updated}), ${image == null ? null : "'" + image + "'"}, '${name}')`);
+            messageId = result.insertId; 
+            await conn.query(`UPDATE conversation SET last_message_id = ${messageId} WHERE id=${conversationId}`);
+        } catch(err) {
+            console.log(err);
+        }
+        
+        io.sockets.in(conversationId).emit('message', ({
+            messageId : messageId,
+            conversationId : conversationId, 
+            userId : userId, 
+            name : name,
+            message : message, 
+            image : image,
+            messageType : messageType, 
+            updated : updated
+        }));
+
+    });
+
+    socket.on('readinfo', async ({conversationId, userId, lastMessageId}) => {        
+
+        const conn = pool.promise();
+        if(lastMessageId == null) {
+            try {
+                const [rows, fields] = await conn.query(`SELECT last_message_id FROM conversation WHERE id = ${conversationId}`);
+                lastMessageId = rows[0].last_message_id;
+            } catch(err) {
+                console.log(err);
+            }
+        }
+        try{
+            await conn.query(`INSERT INTO read_info VALUES(null, ${conversationId}, ${userId}, ${lastMessageId})`);
+        }catch(err){
+            console.log(err);
+        }
+
+        io.sockets.in(conversationId).emit('readinfo', ({
+            conversationId : conversationId, 
+            userId : userId,
+            lastMessageId : lastMessageId
+        }));
+
+    });
+
+    socket.on('disconnect', function() {
+
+        console.log("SOCKETIO disconnect EVENT: ", socket.id, " client disconnect");
+        var rooms = io.sockets.adapter.sids[socket.id];
+        for(var room in rooms) {
+            socket.leave(room);
+        }   
+    });
+
+    socket.on('loadmessage', async ({conversationId, offset, size}) => {
+        const conn = pool.promise();
+        try {
+            const [rows, fields] = await conn.query(`SELECT * FROM (SELECT id,conversation_id,user_id,message_type,message,unix_timestamp(updated) as updated,image,name FROM message WHERE conversation_id=${conversationId} order by id desc) orders limit ${size} offset ${offset}`);
+            socket.emit('loadmessage', ({
+              rows : rows  
+            }));
+
+        } catch(err){
+            console.log(err);
+        }
+    });
+
+    socket.on('loadreadinfo', async({conversationId}) => {
+        const conn = pool.promise();
+        try {
+            const [rows, fields] = await conn.query(`select user_id, max(last_message_id) as last_message_id from read_info where user_id in (select user_id from user_conversation where conversation_id=${conversationId}) and conversation_id=${conversationId} group by user_id`);
+            for(let row of rows) {
+
+                socket.emit('readinfo', ({
+                    conversationId : conversationId,
+                    userId : row.user_id,
+                    lastMessageId : row.last_message_id
+                  }));
+
+            }
+            
+
+        } catch(err){
+            console.log(err);
+        }
+    });
+
+    socket.on('outconversation', async ({userId, name, conversationId}) => {
+
+        const conn = pool.promise();
+        try {
+
+            const [rows, fields] = await conn.query(`SELECT user_count, title FROM conversation WHERE id=${conversationId}`);
+            let userCount = rows[0].user_count;
+            if(userCount == 1) {
+                await conn.query(`DELETE FROM conversation WHERE id = ${conversationId}`);
+                await conn.query(`DELETE FROM user_conversation WHERE conversation_id = ${conversationId}`);
+
+            } else {
+                userCount -= 1;
+                let title = rows[0].title;
+                if(title.indexOf(name)===0) {
+                    title = title.substr(name.length + 1);
+                } else {
+                    title = title.split("," + name).join("");
+                }
+                await conn.query(`UPDATE conversation SET user_count = ${userCount}, title='${title}' WHERE id=${conversationId}`);
+                await conn.query(`DELETE FROM user_conversation WHERE user_id = ${userId} AND conversation_id = ${conversationId}`);
+                await conn.query(`INSERT INTO message VALUES(null, ${conversationId}, ${userId}, 3, '', current_timestamp(), null, '${name}')`);
+
+                io.sockets.to(conversationId).emit('message', ({
+                    conversationId : conversationId,
+                    userId : userId, 
+                    name : name, 
+                    message : '',
+                    messageType : 3,
+                    updated : Date.now()
+                }));
+            }
+
+        } catch(err) {
+            console.log(err);
+        }
+
+    });
+
+    socket.on('uploadImage', ({file, name}) => {
+        console.log('uploadImage ' + name);
+        let path = "../client/public/img/";
+        let filename = Date.now() + name;
+        writeFileSync(path + filename, file);
+        socket.emit('uploadImage', ({
+            filename :  filename
+        }));
+    });
+
+});
+
+
+async function getUnreadCountForConversation(convId, userId) {
+
+    const conn = pool.promise();
+    try{
+        const [rows,fields] = await conn.query(`SELECT MAX(last_message_id) AS last_message_id FROM read_info WHERE user_id=${userId} AND conversation_id=${convId}`);
+        const lastMessageId = rows[0].last_message_id;
+        const [rows2,fields2] = await conn.query(`SELECT COUNT(*) as unreads FROM message WHERE id > ${lastMessageId} AND conversation_id = ${convId} AND message_type != 3`);
+        return rows2[0].unreads;
+
+    }catch(err){
+        console.log(err);
+    }
+    return 0;
+}
+
+
+async function loadConversation(socket, userId) {
+    console.log('loadConversation');
+    const conn = pool.promise();
+    try{
+        const [rows,fields] = await conn.query(`SELECT * FROM conversation WHERE id IN (select conversation_id from user_conversation where user_id = ${userId})`);
+        for(let row of rows) {
+            row.unreads = await getUnreadCountForConversation(row.id, userId);
+            row.userId = userId;
+        }
+        const data = JSON.stringify(rows);
+        socket.emit('loadConversation', ({
+            result : 'success', 
+            data : data
+        }));
+    }catch(err) {
+        console.log(err);
+        socket.emit('loadConversation', ({result:'fail'}));
+    }
+}
+
+
+async function createConversation(socket, info) {
+    console.log('createConversation');
+    let users = JSON.parse(info);
+    console.log(users);
+    let userCount = users.length;
+    if(userCount == 0) {
+        console.log('createConversation::user count = 0');
+        return;
+    }
+    let name = users[0].name;
+    let thumbnail = users[0].profile;
+    if(users.length >= 2) {
+        thumbnail = users[1].profile;
+    }
+    for(var user of users) {
+        if(name === user.name) {
+            continue;
+        }
+        name = name + "," + user.name;
+    }
+
+    let convId = 0;
+    const conn = pool.promise();
+    try {
+        const [rows, fields] = await conn.query(`INSERT conversation VALUES(null, '${thumbnail}', '${name}', 0, ${userCount})`);
+        convId = rows.insertId;
+        console.log("createConversation::convId = " + convId)
+        for(var user of users) {
+            await conn.query(`INSERT user_conversation VALUES(null, ${user.id}, ${convId}, false, true)`);
+        }
+        console.log('createConversation::success');
+    } catch(err) {
+        console.log(err);
+        socket.emit('createConversation',({result:"fail"}) );
+        return;
+    }
+
+    socket.emit('createConversation',({result:"success"}) );
+    
+    
+}
+
+server.listen(1225, function(){
+    console.log('listening on port 1225');
+});
